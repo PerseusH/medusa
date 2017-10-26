85901fc5ffa34224af629cf84b24056a1856cd32
PerseusH <584148102@qq.com>
2017-10-26 15:14:26 +0800

    update react

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,11 @@ var webpack = require('webpack');
 
 module.exports = {//注意这里是exports不是export
     devtool: 'eval-source-map',
-    entry: ['webpack/hot/dev-server', __dirname + "/react/index.js"],//唯一入口文件，就像Java中的main方法
+    entry: [
+        'webpack/hot/dev-server',
+        __dirname + '/react/test.js'],//唯一入口文件，就像Java中的main方法
     output: {//输出目录
-        path: __dirname + "/docs",//打包后的js文件存放的地方
+        path: __dirname + "/static",//打包后的js文件存放的地方
         filename: "app.js"//打包后的js文件名
     },
     module: {
@@ -29,16 +31,16 @@ module.exports = {//注意这里是exports不是export
     },
 
     plugins: [
-        new webpack.HotModuleReplacementPlugin() //热模块替换插件
+        new webpack.HotModuleReplacementPlugin() //热模块HMR替换插件
     ],
 
     //webpack-dev-server
     devServer: {
-        contentBase: './docs', //默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
+        contentBase: './static', //默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录
         // colors: false, //是否在cmd终端中输出彩色日志
         historyApiFallback: true, //Html5 API
         inline: true, //设置为true，当源文件改变时会自动刷新页面
-        port: 8080, //设置默认监听端口，如果省略，默认为"8080"
+        port: 8010, //设置默认监听端口，如果省略，默认为"8080"
         progress: true //显示合并代码进度
     }
 };
