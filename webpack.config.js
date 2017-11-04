6cfb077718f3792a4d13d77bdb0dedcac0d67ee8
PerseusH <584148102@qq.com>
2017-11-05 01:26:22 +0800

    dev1

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@
 
 var webpack = require('webpack');
 
-module.exports = {//注意这里是exports不是export/////////
+module.exports = {//注意这里是exports不是export/////
     devtool: 'eval-source-map',
     entry: [
         'webpack/hot/dev-server',
         __dirname + '/react/test.js'],//唯一入口文件，就像Java中的main方法
-    output: {//输出目录
+    output: {//输出文件夹
         path: __dirname + "/static",//打包后的js文件存放的地方
         filename: "app.js"//打包后的js文件名
     },
