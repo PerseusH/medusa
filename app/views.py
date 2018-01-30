11f0499e6f8ce4242f80d77ed9d6bc2322bccc49
PerseusH <584148102@qq.com>
2018-01-30 19:34:56 +0800

    add web browser cache

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 import os
 import time
+from hashlib import sha1
 from flask import Flask
+from flask import request
+from flask import make_response
 from flask_mako import MakoTemplates
 from flask_mako import render_template
 from libs.NBAGames import NBAGames
@@ -10,11 +13,35 @@ from datamap import datamap
 app = Flask(__name__)
 mako = MakoTemplates(app)
 title_prefix = "A personal blog belongs to PerseusH"
-
+_index_cache = dict()
 
 @app.route("/")
 def index():
-    return _render("index.html")
+    data = _render("index.html")
+    global _index_cache
+    print type(request.headers)
+    print request.headers
+
+    if not _index_cache.get("templ"):
+        _index_cache["templ"] = sha1(repr(data)).hexdigest()
+    datahash = _index_cache["templ"]
+
+    if not _index_cache.get("modifiedtime"):
+        modified_time = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
+        _index_cache["modifiedtime"] = modified_time
+    modified_time = _index_cache["modifiedtime"]
+
+    print request.environ
+    #if request.environ.get("HTTP_IF_NONE_MATCH", None) == datahash:
+    if request.environ.get("HTTP_IF_MODIFIED_SINCE", None) == modified_time:
+        response = make_response("", 304)
+    else:
+        response = make_response(data)
+        response.headers["Content-Type"] = "text/html; charset=utf-8"
+        response.headers["Etag"] = datahash
+        response.headers["Last-Modified"] = modified_time
+    
+    return response
 
 
 @app.route("/hello/")
@@ -26,6 +53,7 @@ def hello(username=""):
 
 @app.route("/nba/")
 def nba():
+    print request.headers
     today = time.strftime("%Y-%m-%d", time.localtime())
     return _render("nba.html", "NBAGames", **{"games": NBAGames(), "today": today})
 
